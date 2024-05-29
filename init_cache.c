c1c3f491f612a0ea5d5fe34038a60f3ae7a223d9
Denis-Kuso <sourcework.online@keemail.me>
2024-05-29 20:55:28 +0100

    implement initialisation of a cache

---
+++ init_cache.c
@@ -0,0 +1,48 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "parse-address.c"
+
+// Tag and valid bit used for retrieval of cached entry
+// LRU represent number of cache lookups since last use
+typedef struct{
+    /*
+    * Identification
+    */
+    int tag;
+    /*
+    * Ticks since last used
+    */
+    int LRU;
+    /*
+    * true indicates valid entry
+    */
+    bool valid;
+}cache_line;
+
+cache_line **init_cache(int setBits, int lines){
+
+    int sets = 1 << setBits;
+    cache_line **cache;
+    cache = malloc(sets * sizeof(int *));
+    if (!cache) {
+        fprintf(stderr, "No memory! Cannot allocate cache sets\n");
+        return EXIT_FAILURE;
+    }
+    for (int i = 0; i < sets; i++) {
+        cache[i] = (cache_line*)malloc(lines * sizeof(cache_line));
+        if (!cache[i]) {
+            fprintf(stderr, "No memory! Cannot allocate cache lines\n");
+            return EXIT_FAILURE;
+        }
+    }
+    // init empty cache
+    for (int r = 0; r < sets; r++) {
+        for (int n = 0; n < lines; n++) {
+            cache[r][n].valid = false;
+            cache[r][n].tag = 0;
+            cache[r][n].LRU = 0;
+        }
+    }
+    return cache;
+}
